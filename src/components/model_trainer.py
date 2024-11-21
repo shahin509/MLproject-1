725f8f1256a451a4af9e6b3fb1963e3d76b9b98e
shahin509 <hossainshahin230@gmail.com>
2024-11-21 18:16:18 +0600

    data_ingestion complete

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -51,62 +51,42 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor()
             }
 
-            params = {
-
-                "Random Forest": {
-                    'n_estimators': [50, 100, 200, 300],  # Increased focus on realistic estimator counts
-                    'max_depth': [None, 10, 20, 30, 50],  # Same as Decision Tree for depth
-                    'min_samples_split': [2, 5, 10],  # Matches Decision Tree's split
-                    'min_samples_leaf': [1, 2, 4],
-                    'bootstrap': [True, False],  # Bootstrap sampling for bagging
-                },
-
+            params={
                 "Decision Tree": {
-                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error'],  # Removed 'poisson' as it's usually for count data
-                    'splitter': ['best', 'random'],  # Useful for exploring different splitting strategies
-                    'max_depth': [None, 10, 20, 30, 50],  # Limits to prevent overfitting
-                    'min_samples_split': [2, 5, 10],  # Controls minimum samples to split a node
-                    'min_samples_leaf': [1, 2, 4],  # Controls minimum samples for a leaf node
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
                 },
-
-                "Gradient Boosting": {
-                    'learning_rate': [0.01, 0.05, 0.1, 0.2],  # Added slightly higher learning rates
-                    'n_estimators': [50, 100, 200, 300],  # Consistent with Random Forest
-                    'subsample': [0.7, 0.8, 0.9, 1.0],  # Slightly tighter range for subsampling
-                    'max_depth': [3, 5, 7],  # Typical depths for Gradient Boosting
-                    'min_samples_split': [2, 5],
-                    'min_samples_leaf': [1, 2],
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
                 },
-
-                "Linear Regression": {
-                    # Linear regression usually doesn't have hyperparameters but can include normalization
-                    'fit_intercept': [True, False],
-                    'normalize': [True, False],  # Deprecated in some libraries but relevant in older versions
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
                 },
-
-                "XGBRegressor": {
-                    'learning_rate': [0.01, 0.05, 0.1, 0.2],
-                    'n_estimators': [50, 100, 200, 300],
-                    'max_depth': [3, 5, 7],  # Controls tree complexity
-                    'subsample': [0.7, 0.8, 0.9, 1.0],
-                    'colsample_bytree': [0.7, 0.8, 0.9, 1.0],  # Feature subsampling
-                    'reg_alpha': [0, 0.1, 1],  # L1 regularization
-                    'reg_lambda': [1, 1.5, 2],  # L2 regularization
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
                 },
-
-                "CatBoosting Regressor": {
-                    'depth': [6, 8, 10],
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
                     'learning_rate': [0.01, 0.05, 0.1],
-                    'iterations': [100, 200, 300],  # Increased from lower values for more training iterations
-                    'l2_leaf_reg': [3, 5, 7],  # Regularization parameter
+                    'iterations': [30, 50, 100]
                 },
-
-                "AdaBoost Regressor": {
-                    'learning_rate': [0.01, 0.05, 0.1, 0.2],  # Removed overly small/large values
-                    'n_estimators': [50, 100, 200, 300],
-                    'loss': ['linear', 'square', 'exponential'],  # Commonly used loss functions
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
                 }
-
+                
             }
 
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test, models=models,param=params)
@@ -130,7 +110,9 @@ class ModelTrainer:
             predicted=best_model.predict(X_test)
             r2_square = r2_score(y_test, predicted)
 
+            # return best_model,r2_square
             return best_model,r2_square
+
             
         except Exception as e:
             raise CustomException(e,sys)
